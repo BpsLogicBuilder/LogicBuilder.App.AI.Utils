c06d893713219e2542944bebedbee3567b536a16
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-09-22 14:51:48 -0400

    AB#229: Fix CodeCov recommendations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+* 2026-09-22 - AB#229: Fix CodeCov recommendations.
 * 2026-09-22 - AB#229: Fix SonarCloud recommendations.
 * 2026-09-22 - AB#229: Add handler and creator classes to support the SignalR Hub.
 * 2026-09-21 - AB#229: Add CI CD workflows.
--- LogicBuilder.App.AI.Utils/Parameters/DummyAIUtilsParametersConstructor.cs
+++ LogicBuilder.App.AI.Utils/Parameters/DummyAIUtilsParametersConstructor.cs
@@ -1,6 +1,9 @@
-﻿namespace LogicBuilder.App.AI.Utils.Parameters
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LogicBuilder.App.AI.Utils.Parameters
 {
 #pragma warning disable CS9113//used for configuration by reflection
+    [ExcludeFromCodeCoverage]
     public class DummyAIUtilsParametersConstructor(
         AgentParameters agentParameters,
         McpToolParameters mcpToolParameters,
